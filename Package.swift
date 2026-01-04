78e886258437061db01403a233f339f3d5f75a35
Ethan Parker <woody@mddcompany.com>
2026-01-04 20:49:52 +0900

    Feat: Update SwiftSyntax dependency URL

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .library(name: "InnoDI", targets: ["InnoDI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "602.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "602.0.0"),
     ],
     targets: [
         .target(
